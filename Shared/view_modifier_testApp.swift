604b903f321ecbc424f5a1f69aff2662c28a5d18
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-06-28 17:24:09 +0300

    Initial Commit

---
+++ Shared/view_modifier_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  view_modifier_testApp.swift
+//  Shared
+//
+//  Created by Iiro Alhonen on 28.6.2022.
+//
+
+import SwiftUI
+
+@main
+struct view_modifier_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
